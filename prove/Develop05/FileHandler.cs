c2b743d3ec809db1054d6fde28249480a29c3345
daphnet123 <try22002@byui.edu>
2024-11-19 18:45:00 -0600

    maybe final edits?

--- prove/Develop05/FileHandler.cs
+++ prove/Develop05/FileHandler.cs
@@ -1,29 +1,32 @@
 using System.IO;
-using Newtonsoft.Json;
+using System.Text.Json;
 
 public class FileHandler
 {
     private const string GoalFilePath = "goals.json";
     private const string ProgressFilePath = "progress.json";
 
-    private static readonly JsonSerializerSettings JsonSettings = new JsonSerializerSettings
+    private static readonly JsonSerializerOptions JsonOptions = new JsonSerializerOptions
     {
-        TypeNameHandling = TypeNameHandling.Auto,
-        Formatting = Formatting.Indented
+        WriteIndented = true, 
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase 
     };
 
     public void SaveData(GoalManager goalManager, ProgressTracker progressTracker)
     {
-        File.WriteAllText(GoalFilePath, JsonConvert.SerializeObject(goalManager.GetGoals(), JsonSettings));
-        File.WriteAllText(ProgressFilePath, JsonConvert.SerializeObject(progressTracker, JsonSettings));
+        File.WriteAllText(GoalFilePath, JsonSerializer.Serialize(goalManager.GetGoals(), JsonOptions));
+        File.WriteAllText(ProgressFilePath, JsonSerializer.Serialize(progressTracker, JsonOptions));
     }
 
     public (GoalManager, ProgressTracker) LoadData()
     {
         if (File.Exists(GoalFilePath) && File.Exists(ProgressFilePath))
         {
-            var goals = JsonConvert.DeserializeObject<List<Goal>>(File.ReadAllText(GoalFilePath), JsonSettings);
-            var progress = JsonConvert.DeserializeObject<ProgressTracker>(File.ReadAllText(ProgressFilePath), JsonSettings);
+            var goalsJson = File.ReadAllText(GoalFilePath);
+            var progressJson = File.ReadAllText(ProgressFilePath);
+
+            var goals = JsonSerializer.Deserialize<List<Goal>>(goalsJson, JsonOptions);
+            var progress = JsonSerializer.Deserialize<ProgressTracker>(progressJson, JsonOptions);
 
             GoalManager goalManager = new GoalManager();
             foreach (var goal in goals)
